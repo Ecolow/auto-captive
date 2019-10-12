7d685836a29c5a5a04486bea3a5b7d331f173db7
Ecolow <sylvainkerleroux@gmail.com>
2019-10-12 17:00:08 +0100

    Refactoring and improving resilience to networking issues.

--- PretCustomer.py
+++ PretCustomer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,44 +10,60 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import random 
 import string 
-import subprocess 
+
 
 TEST_URL = "https://www.fadvisor.net/blog/"
 
+params = {
+        'firstName': "",
+        'LastName' : "",
+        'email'    : "",
+        }
+
+
 # generating random strings of min/max 4/10 charracters for first and last name
-first_name = ''.join([random.choice(string.ascii_letters) for n in range(random.randint(4, 10))])
-last_name  = ''.join([random.choice(string.ascii_letters) for n in range(random.randint(4, 10))])
-email = first_name + last_name + "@gmail.com"
+def generate_random_string(min, max):
+    return ''.join([random.choice(string.ascii_letters) for n in range(random.randint(min, max))])
 
+def input_params(browser):
+    for element, value in params.items():
+        field = browser.find_element_by_name(element)
+        field.send_keys(value)
 
-# make the driver headless 
-options = Options()
-options.headless = True
+def extra_params(browser):
+     cbox = browser.find_element_by_xpath("/html/body/form/div/div[2]/div/table/tbody/tr/td/div/div[9]/div/label[1]")
+     actions = ActionChains(browser)
+     actions.move_to_element(cbox).click().perform()
 
-browser = webdriver.Firefox(options=options, service_log_path='/home/echologic/debug.log')
+def submit_form(browser):
+    # should add to wait for the sign in button
+    signInButton = browser.find_element_by_class_name("skyfii-registrationSubmit-button")
+    signInButton.click()
 
-# request url
-browser.get(TEST_URL)
+# Checks if the captive portal is active
+def is_authenticated(browser):
+    try: 
+        browser.get(TEST_URL)
+        if browser.current_url != TEST_URL:
+            return False
+        else:
+            return True
+    except Exception as e:
+        print(e)
+        return False
+    
 
-# if redirected to login page
-if browser.current_url != TEST_URL:
-    print("Captive portal active, attempting to authenticate with ", first_name, last_name, email)
-    subprocess.Popen(['notify-send', "Captive Portal Auto-login", "Attempting authentication..."])
-
+# Authentication procedure
+def authenticate(browser, params):
     try:
-        fname = browser.find_element_by_name("firstName")
-        fname.send_keys(first_name)
-        lname  = browser.find_element_by_name("LastName")
-        lname.send_keys(last_name)
-        mail  = browser.find_element_by_name("email")
-        mail.send_keys(email)
-        cbox = browser.find_element_by_xpath("/html/body/form/div/div[2]/div/table/tbody/tr/td/div/div[9]/div/label[1]")
-        actions = ActionChains(browser)
-        actions.move_to_element(cbox).click().perform()
+        # Input parameters into the form
+        input_params(browser)
+        extra_params(browser)
+        
+        # Waiting for the submit button
         time.sleep(2)
-        signInButton = browser.find_element_by_class_name("skyfii-registrationSubmit-button")
-        signInButton.click()
-
+        submit_form(browser)
+             
         # wait for the alert and accept it 
         WebDriverWait(browser, 2).until(EC.alert_is_present(),
                 'Waiting for insecure connection alert to appear...')
@@ -54,19 +72,48 @@ if browser.current_url != TEST_URL:
 
         # wait for redirect to finish
         time.sleep(3)
-        #WebDriverWait(browser, 2).until(EC.url_to_be('https://www.pret.co.uk/en-gb', 
-        #    'Waiting for redirection to finish...'))
     
     except Exception as e:
         print(e)
 
-else : 
-    print("Already authenticated")
 
-# Notify user of successful authentication
-subprocess.Popen(['notify-send', "Captive Portal Auto-login", "Authenticated successfully to Pret a Manger"])
+def main():
+    
+    # disable cache
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("browser.cache.disk.enable", False)
+    profile.set_preference("browser.cache.memory.enable", False)
+    profile.set_preference("browser.cache.offline.enable", False)
+    profile.set_preference("network.http.use-cache", False)
+    
+    # make the driver headless 
+    options = Options()
+    options.headless = True
+    options.profile = profile
 
-# Close the browser gracefully
-browser.quit()
+    # initialise the driver 
+    browser = webdriver.Firefox(options=options, service_log_path='/home/echologic/debug.log')
 
-print("Authenticated Successfully")
+    # to be change with realistic credentials from db 
+    params['firstName'] = generate_random_string(4, 10)
+    params['LastName'] = generate_random_string(4, 10)
+    params['email'] = params['firstName'] + params['LastName'] + "@gmail.com"
+    
+    print("Authenticating with; ", params)
+    
+    # Retry clause
+    authenticated = is_authenticated(browser)
+    while not authenticated:
+        authenticate(browser, params)
+        authenticated = is_authenticated(browser)
+        if authenticated:
+            break
+        time.sleep(1)
+        print("Retryinng")
+
+    print("Authenticated Successfully")
+    
+    # Close the browser gracefully
+    browser.quit()
+
+main()
